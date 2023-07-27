1f89bb078a57568aecc73f5e9233ff9b9e445906
Adrien Bérard <adrienberard@hotmail.fr>
2023-07-27 11:44:56 +0200

    supp des chemins d'accès

---
+++ main/app/data_list.py
@@ -0,0 +1,22 @@
+
+import app.utils_format as func
+
+
+sites = [
+{"src": "site","name" : "1AG1", "metal" : "AG" , "url":"https://www.cookson-clal.com/cours/", "func": "extract_1AG1", "unit" : "KG", "devise": "€", "format_func" : func.format_value_1AG1},
+{"src": "site","name" : "1AG3" , "metal" : "AG" , "url": "https://www.westmetall.com/en/markdaten.php?action=table&field=Ag", "func": "extract_1AG3", "unit" : "KG", "devise": "€", "format_func" : func.format_value_1AG3},
+{"src": "site","name" : "1AU3" , "metal" : "AU" ,"url" : "https://www.cookson-clal.com/cours/", "func": "extract_1AU3", "unit" : "KG", "devise": "€", "format_func" : func.format_value_1AU3},
+{"src": "site","name" : "2M37" , "metal" : "M37" ,"url" : "https://www.westmetall.com/en/markdaten.php?action=table&field=MB_MS_63_37", "func": "extract_2M37", "unit" : "100KG", "devise": "€", "format_func" : func.format_value_2M37},
+{"src": "site","name" : "3AL1" , "metal" : "AL" ,"url" : "https://www.westmetall.com/en/markdaten.php?action=average&field=LME_AI_cash", "func": "extract_3AL1", "unit" : "TO", "devise": "$", "format_func" : func.format_value_3AL1},
+{"src": "site","name" : "3CU1" , "metal" : "CU" ,"url" : "https://www.westmetall.com/en/markdaten.php?action=table&field=LME_Cu_cash", "func": "extract_3CU1", "unit" : "TO", "devise": "$", "format_func" : func.format_value_3CU1},
+{"src": "site","name" : "3CU3" , "metal" : "CU" ,"url" : "https://www.westmetall.com/en/markdaten.php?action=table&field=WI_Cu", "func": "extract_3CU3", "unit" : "100KG", "devise": "€", "format_func" : func.format_value_3CU3},
+{"src": "pdf" ,"name" : "2CUB" , "metal" : "CUB" ,"url" : "https://materion.de.com:443/-/media/files/german/metalvaluepostingfile.pdf", "func": "extract_2CUB", "unit" : "KG", "devise": "€", "name_pdf" : "metalvaluepostingfile.pdf", "format_func" : func.format_value_2CUB},
+{"src": "site","name" : "3NI1" , "metal" : "NI" ,"url" : "https://www.kme.com/fr/services/cours-des-metaux", "func": "extract_3NI1", "unit" : "TO", "devise": "$", "format_func" : func.format_value_3NI1},
+{"src": "site","name" : "3SN1" , "metal" : "SN" ,"url" : "https://www.kme.com/fr/services/cours-des-metaux", "func": "extract_3SN1", "unit" : "TO", "devise": "$", "format_func" : func.format_value_3SN1},
+{"src": "ext" ,"name" : "1AG2" , "metal" : "AG" ,"url" : "https://www.lbma.org.uk/prices-and-data/precious-metal-prices#", "func": "extract_1AG2", "unit": "OZ", "devise": "$", "format_func" : func.format_value_1AG2},
+{"src": "ext" ,"name" : "1AU2" , "metal" : "AU" ,"url" : "https://www.lbma.org.uk/prices-and-data/precious-metal-prices#", "func": "extract_1AU2", "unit": "OZ", "devise": "$", "format_func" : func.format_value_1AU2},
+{"src": "site" ,"name" : "2B16" , "metal" : "B16" ,"url" : "https://www.westmetall.com/en/markdaten.php?action=average&field=LME_AI_cash", "func": "extract_2B16", "unit": "100KG", "devise": "€", "format_func" : func.format_value_2B16},
+{"src": "site" ,"name" : "2M30" , "metal" : "M30" ,"url" : "https://www.wieland.com/en/resources/metal-information#metal-information", "func": "extract_2M30", "unit": "100KG", "devise": "€", "format_func" : func.format_value_2M30},
+{"src": "ext" ,"name" : "3ZN1" , "metal" : "ZN" ,"url" : "https://www.westmetall.com/en/markdaten.php?action=table&field=LME_Zn_cash", "func": "extract_3ZN1", "unit": "TO", "devise": "$", "format_func" : func.format_value_3ZN1},
+{"src": "ext" ,"name" : "2360" , "metal" : "360" ,"url" : "https://www.westmetall.com/en/markdaten.php?action=table&field=LME_Zn_cash", "func": "extract_2360", "unit": "KG", "devise": "€", "format_func" : func.format_value_2360},
+]
---
+++ main/app/utils_scrapping.py
@@ -0,0 +1,356 @@
+
+import configparser
+from PyPDF2 import PdfReader
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from app.data_list import sites
+import os
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from .config import get_config_value, get_pdf_path, set_config_value
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+
+# Extraction données Cookson pour 1AG1 (EL)
+def extract_1AG1(soup):
+    """Extraire les données de la table Cookson et les ajouter au classeur Excel"""
+    table = soup.find("table")
+    rows = soup.find_all("tr")
+    second_row = rows[3]
+
+    # Trouver la quatrième colonne de la table dans la deuxième ligne
+    columns = second_row.find_all("td")
+    last_column = columns[3]
+
+    # Extraire le texte de la quatrième colonne
+    data = last_column.text.strip()
+    formatted_data = data.replace('€', '')
+    return formatted_data
+
+# Extraction données Cookson pour 1AU3 (EL)
+def extract_1AU3(soup):
+    """Extraire les données de la table Cookson et les ajouter au classeur Excel"""
+    table = soup.find("table")
+    rows = soup.find_all("tr")
+    second_row = rows[2]
+
+    # Trouver la quatrième colonne de la table dans la deuxième ligne
+    columns = second_row.find_all("td")
+    last_column = columns[3]
+
+    # Extraire le texte de la quatrième colonne
+    data = last_column.text.strip()
+    formatted_data = data.replace('.', ',').replace('€', '').replace(' ','')
+    return formatted_data
+
+
+# Extraction données pour 1AG3 (EL)
+def extract_1AG3(soup):
+    """Extraire les données de la table Cookson et les ajouter au classeur Excel"""
+    table = soup.find("table")
+    rows = soup.find_all("tr")
+    second_row = rows[1]
+
+    # Trouver la quatrième colonne de la table dans la deuxième ligne
+    columns = second_row.find_all("td")
+    fourth_column = columns[1]
+
+    # Extraire le texte de la quatrième colonne
+    data = fourth_column.text.strip()
+    formatted_data = data.replace('.', ',')
+    return formatted_data
+
+# Extraction données pour 2M37 (EL)
+def extract_2M37(soup):
+    """Extraire les données de la table Cookson et les ajouter au classeur Excel"""
+    table = soup.find("table")
+    rows = soup.find_all("tr")
+    second_row = rows[1]
+
+    # Trouver la quatrième colonne de la table dans la deuxième ligne
+    columns = second_row.find_all("td")
+    fourth_column = columns[1]
+
+    # Extraire le texte de la quatrième colonne
+    data = fourth_column.text.strip()
+    formatted_data = data.replace('.', ',')
+    return formatted_data
+
+# Extraction données pour 3AL1 (EL)
+def extract_3AL1(soup):
+    """Extraire les données de la table Cookson et les ajouter au classeur Excel"""
+    table = soup.find("table")
+    rows = soup.find_all("tr")
+    second_row = rows[6]
+
+    # Trouver la quatrième colonne de la table dans la deuxième ligne
+    columns = second_row.find_all("td")
+    fourth_column = columns[1]
+
+    # Extraire le texte de la quatrième colonne
+    data = fourth_column.text.strip()
+    formatted_data = data.replace(',', '').replace('.', ',')
+    return formatted_data
+
+# Extraction données pour 3CU1 (EL)
+def extract_3CU1(soup):
+    """Extraire les données de la table Cookson et les ajouter au classeur Excel"""
+    table = soup.find("table")
+    rows = soup.find_all("tr")
+    second_row = rows[1]
+
+    # Trouver la quatrième colonne de la table dans la deuxième ligne
+    columns = second_row.find_all("td")
+    fourth_column = columns[1]
+
+    # Extraire le texte de la quatrième colonne
+    data = fourth_column.text.strip()
+    formatted_data = data.replace(',', '').replace('.', ',')
+    return formatted_data
+
+# Extraction données pour 3CU3 (EL)
+def extract_3CU3(soup):
+    """Extraire les données de la table Cookson et les ajouter au classeur Excel"""
+    table = soup.find("table")
+    rows = soup.find_all("tr")
+    second_row = rows[1]
+
+    # Trouver la quatrième colonne de la table dans la deuxième ligne
+    columns = second_row.find_all("td")
+    fourth_column = columns[1]
+
+    # Extraire le texte de la quatrième colonne
+    data = fourth_column.text.strip()
+    formatted_data = data.replace('.', ',')
+    return formatted_data
+
+# Extraction données pour 3NI1 (EL)
+def extract_3NI1(soup):
+    """Extraction NICKEL Ligne 2, Valeur Colonne 3"""
+    table = soup.find('table', class_='table table-condensed table-hover table-striped')
+
+    rows = soup.find_all('tr')
+    second_row = rows[2]
+
+    columns = second_row.find_all('td')
+    fourth_column = columns[2]
+    data = fourth_column.text.strip()
+    formatted_data = data.replace('.', '').replace('¹', '')
+    return formatted_data
+
+# Extraction données pour 3SN1 (EL)
+def extract_3SN1(soup):
+    """Extraction ETAIN Ligne 3, Valeur Colonne 3"""
+    table = soup.find('table', class_='')
+    rows = soup.find_all('tr')
+    second_row = rows[3]
+
+    columns = second_row.find_all('td')
+    fourth_column = columns[2]
+    data = fourth_column.text.strip()
+
+    formatted_data = data.replace('.', '').replace('¹', '')
+    return formatted_data
+
+def extract_2CUB(soup):
+    """Extraire les données de la table Materion et les ajouter au classeur Excel"""
+
+    pdf_path = get_config_value('main', 'pdf_path')
+    name_pdf = get_config_value('main', 'name_pdf')
+
+    if not pdf_path:
+        pdf_path = os.getcwd()
+
+    path = f"{pdf_path}/{name_pdf}"
+    try:
+        with open(path, 'rb') as pdf_materion:
+            reader_materion = PdfReader(pdf_materion)
+            page_materion = reader_materion.pages[0]
+            text_materion = page_materion.extract_text()
+            print('PDF lu')
+
+            lines = text_materion.split('\n')
+
+            alloy_line = None
+            for line in lines:
+                if line.startswith('Alloy 25'):
+                    alloy_line = line
+                    break
+
+            if alloy_line is not None:
+                # Récupérer la valeur de la 4ème colonne
+                columns = alloy_line.split()
+                if len(columns) >= 4:
+                    price_eur = columns[4]
+                else:
+                    price_eur = None
+
+                formatted_data = price_eur.replace('.', ',')
+                return formatted_data
+    except FileNotFoundError:
+        print(f"Le fichier PDF '{name_pdf}' n'a pas été trouvé. Passage à autre chose.")
+        formatted_data = 'err'
+        return formatted_data
+
+def extract_2360(soup):
+    """Extraire les données de la table Materion et les ajouter au classeur Excel"""
+
+    pdf_path = get_config_value('main', 'pdf_path')
+    name_pdf = get_config_value('main', 'name_pdf')
+
+    if not pdf_path:
+        pdf_path = os.getcwd()
+
+    path = f"{pdf_path}/{name_pdf}"
+    try:
+        with open(path, 'rb') as pdf_materion:
+            reader_materion = PdfReader(pdf_materion)
+            page_materion = reader_materion.pages[0]
+            text_materion = page_materion.extract_text()
+            print('PDF lu')
+
+            lines = text_materion.split('\n')
+
+            alloy_line = None
+            for line in lines:
+                if line.startswith('Alloy 360'):
+                    alloy_line = line
+                    break
+
+            if alloy_line is not None:
+                # Récupérer la valeur de la 4ème colonne
+                columns = alloy_line.split()
+                if len(columns) >= 4:
+                    price_eur = columns[4]
+                else:
+                    price_eur = None
+
+                formatted_data = price_eur.replace('.', ',')
+                return formatted_data
+    except FileNotFoundError:
+        print(f"Le fichier PDF '{name_pdf}' n'a pas été trouvé. Passage à autre chose.")
+        formatted_data = 'err'
+        return formatted_data
+
+# Extraction données lbma pour 1AG2 (EL)
+def extract_1AG2(soup):
+    formatted_data = "init"
+    
+    path_driver_chrome = get_config_value('main', 'path_driver_chrome')
+    s=Service(path_driver_chrome)
+    browser = webdriver.Chrome(service=s)
+    url= 'https://www.lbma.org.uk/prices-and-data/precious-metal-prices#/table'
+    try:
+        browser.get(url)
+        browser.maximize_window()
+        time.sleep(5)
+        table_path = "/html/body/div[1]/main/div[1]/div/div/div/div/div[2]/div/div[2]/div[4]/table"
+
+        table = browser.find_elements(By.XPATH, "/html/body/div[1]/main/div[1]/div/div/div/div/div[2]/div/div[2]/div[4]/table")
+        rows = browser.find_elements(By.XPATH, '/html/body/div[1]/main/div[1]/div/div/div/div/div[2]/div/div[2]/div[4]/table/tbody/tr[1]')
+        drop = browser.find_elements(By.CLASS_NAME, 'dropdown-toggle')
+        drop[0].click()
+        a_drop = browser.find_elements(By.LINK_TEXT, 'Silver')
+        a_drop[0].click()
+        time.sleep(4)
+
+        for row in rows:
+            cells = row.find_elements(By.XPATH, '/html/body/div[1]/main/div[1]/div/div/div/div/div[2]/div/div[2]/div[4]/table/tbody/tr[1]/td[2]')
+            for cell in (cells):
+                formatted_data = cell.text.replace('.', ',')
+
+    except NoSuchElementException:
+        print('Erreur : élément non trouvé')
+        formatted_data = None
+    except TimeoutException:
+        print('Erreur : délai d\'attente dépassé')
+        formatted_data = None
+    finally:
+        return formatted_data
+
+
+# Extraction données lbma pour 1AU2 (EL)
+def extract_1AU2(soup):
+
+    formatted_data = "init"
+    try:
+        path_driver_chrome = get_config_value('main', 'path_driver_chrome')
+        s=Service(path_driver_chrome)
+        browser = webdriver.Chrome(service=s)
+        url='https://www.lbma.org.uk/prices-and-data/precious-metal-prices#/table'
+        browser.get(url)
+        browser.maximize_window()
+        time.sleep(5)
+        table_path = "/html/body/div[1]/main/div[1]/div/div/div/div/div[2]/div/div[2]/div[4]/table"
+
+        table = browser.find_elements(By.XPATH, "/html/body/div[1]/main/div[1]/div/div/div/div/div[2]/div/div[2]/div[4]/table")
+        rows = browser.find_elements(By.XPATH, '/html/body/div[1]/main/div[1]/div/div/div/div/div[2]/div/div[2]/div[4]/table/tbody/tr[1]')
+
+        for row in rows:
+            cells = row.find_elements(By.XPATH, '/html/body/div[1]/main/div[1]/div/div/div/div/div[2]/div/div[2]/div[4]/table/tbody/tr[1]/td[3]')
+            for cell in (cells):
+                formatted_data = cell.text.replace('.', ',')
+                
+    except NoSuchElementException:
+        print("Erreur : élément non trouvé")
+        formatted_data = None
+    except TimeoutException:
+        print("Erreur : délai d'attente dépassé")
+        formatted_data = None
+
+
+    finally:
+        print(formatted_data)
+        return formatted_data
+
+# Extraction données 2M30
+def extract_2M30(soup):
+    table = soup.find('table', class_='metalinfo-table table-metal-prices')
+
+    rows = soup.find_all('tr')
+    second_row = rows[23]
+
+    columns = second_row.find_all('td')
+    fourth_column = columns[1]
+    data = fourth_column.text.strip()
+    formatted_data = data.replace(',', '').replace('.', ',')
+
+    return formatted_data
+
+# Extraction données 2B16
+def extract_2B16(soup):
+    """Extraire les données de la table Cookson et les ajouter au classeur Excel"""
+    table = soup.find("table")
+    rows = soup.find_all("tr")
+    second_row = rows[26]
+
+    # Trouver la quatrième colonne de la table dans la deuxième ligne
+    columns = second_row.find_all("td")
+    fourth_column = columns[1]
+
+    # Extraire le texte de la quatrième colonne
+    data = fourth_column.text.strip()
+    formatted_data = data.replace(',', '').replace('.', ',')
+    return formatted_data
+
+# Extraction données 3ZN1
+def extract_3ZN1(soup):
+    """Extraire les données de la table Cookson et les ajouter au classeur Excel"""
+    table = soup.find("table")
+    rows = soup.find_all("tr")
+    second_row = rows[1]
+
+    # Trouver la quatrième colonne de la table dans la deuxième ligne
+    columns = second_row.find_all("td")
+    fourth_column = columns[1]
+
+    # Extraire le texte de la quatrième colonne
+    data = fourth_column.text.strip()
+    formatted_data = data.replace(',', '').replace('.', ',')
+    return formatted_data
