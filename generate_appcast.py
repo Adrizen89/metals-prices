6ce2f15184079eefdc07f48e8ea01931dceeb6c8
Adrien Bérard <adrienberard@hotmail.fr>
2023-11-06 15:12:17 +0100

    modif xml

--- generate_appcast.py
+++ generate_appcast.py
@@ -10,13 +10,22 @@ def generate_appcast():
     version = version_info['version']
     url = version_info['url']  # Assurez-vous que cette URL pointe vers l'exécutable dans les assets de release
 
-    appcast_xml = f"""
-    <item>
-        <title>Version {version}</title>
-        <sparkle:version>{version}</sparkle:version>
-        <enclosure url="{url}" sparkle:version="{version}" type="application/octet-stream" />
-    </item>
-    """
+    appcast_xml = f"""<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0" xmlns:sparkle="http://www.andymatuschak.org/xml-namespaces/sparkle" xmlns:dc="http://purl.org/dc/elements/1.1/">
+    <channel>
+        <title>Votre application Updates</title>
+        <link>{url}</link>
+        <description>Les mises à jour les plus récentes pour Votre application.</description>
+        <language>fr</language>
+        <item>
+            <title>Version {version}</title>
+            <enclosure url="{url}"
+                       sparkle:version="{version}"
+                       sparkle:shortVersionString="{version}"
+                       type="application/octet-stream"/>
+        </item>
+    </channel>
+</rss>"""
 
     # Écrire le fichier Appcast XML
     with open('docs/appcast.xml', 'w') as f:
